9622b09bb8e73729cc08fac60e35e6ac14634fcf
Samuel <smarouba2@gmail.com>
2022-06-02 09:53:36 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  if (blocks > 42) {
+    return blocks - 42
+  } else {
+    return 42 - blocks
+  }
+}
+function distanceFromHqInFeet(block) {
+  let distanceInBlocks = distanceFromHqInBlocks(block)
+  console.log('block', distanceInBlocks)
+  return distanceInBlocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+  return (start - destination) * 264
+} else {
+  return (destination - start) * 264
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let feet = distanceTravelledInFeet(start, destination)
+  if (feet <= 400) {
+    return 0
+  } else if (feet > 400 && feet < 2000) {
+    return (feet - 400) * .02
+  } else if (feet >= 2000 && feet <= 2500) {
+    return 25
+  } else if (feet > 2500) {
+    return 'cannot travel that far'
+  }
+}
